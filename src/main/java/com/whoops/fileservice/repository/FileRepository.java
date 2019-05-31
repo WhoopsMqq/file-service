be581b079c0935845bd56eff1a5b25279f51123b
whoops <whoops_mqq@163.com>
2019-05-31 14:20:39 +0800

    git 提交测试

---
+++ src/main/java/com/whoops/fileservice/repository/FileRepository.java
@@ -0,0 +1,13 @@
+package com.whoops.fileservice.repository;
+
+import com.whoops.fileservice.domain.File;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * File 存储库.
+ * 
+ * @since 1.0.0 2017年3月28日
+ * @author <a href="https://waylau.com">Way Lau</a> 
+ */
+public interface FileRepository extends MongoRepository<File, String> {
+}
